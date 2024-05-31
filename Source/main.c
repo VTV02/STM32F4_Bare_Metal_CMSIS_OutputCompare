98c680c7dc32a9a190490254e003a34e66d530e2
VTV02 <93756924+VTV02@users.noreply.github.com>
2024-05-31 14:18:57 +0700

    Create main.c

---
+++ Source/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<stdint.h>
+#include<stm32f411xe.h>
+#include <uart.h>
+#include <adc.h>
+#include <systick.h>
+#include <tim.h>
+
+
+int main(void)
+{
+	tim2_pa5output_compare();
+	while(1)
+	{
+
+
+	}
+}
+
